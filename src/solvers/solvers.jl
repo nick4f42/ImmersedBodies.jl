nick4f42/ImmersedBoundaryProjection.jl

Add FlowConditions type and FreestreamFlow struct
Combine the `freestream` and `Re` arguments to `PsiOmegaFluidGrid` into
a separate struct `FreestreamFlow`. This will also allow the
`UniformGrid` to choose a default gridstep based on the
`FreestreamFlow`.

src/solvers/solvers.jl
@@ -3,8 +3,9 @@ module Solvers
 using ...ImmersedBoundaryProjection
 using ...ImmersedBoundaryProjection.Bodies
 using ...ImmersedBoundaryProjection.Quantities
+import ...ImmersedBoundaryProjection: advance!, solve!, statetype, solvertype
 import ...ImmersedBoundaryProjection:
-    timestep_scheme, advance!, solve!, statetype, solvertype, timevalue, timeindex
+    timevalue, timeindex, timestep_scheme, conditions, discretized
 import ...ImmersedBoundaryProjection.Bodies:
     body_segment_length, bodypanels, prescribe_motion!
 
@@ -16,7 +17,8 @@ using LinearMaps
 using StaticArrays
 using FunctionWrappers: FunctionWrapper
 
-export PsiOmegaFluidGrid, UniformGrid, MultiLevelGrid, gridstep, sublevel, baselevel
+export FreestreamFlow, PsiOmegaFluidGrid, UniformGrid, MultiLevelGrid
+export gridstep, sublevel, baselevel
 
 include("fluids.jl")
 

nick4f42/ImmersedBodies.jl

src/solvers/solvers.jl
@@ -3,8 +3,8 @@ module Solvers
 using ...ImmersedBodies
 using ...ImmersedBodies.Bodies
 using ...ImmersedBodies.Quantities
-import ...ImmersedBodies:
-    timestep_scheme, advance!, solve!, statetype, solvertype, timevalue, timeindex
+import ...ImmersedBodies: advance!, solve!, statetype, solvertype
+import ...ImmersedBodies: timevalue, timeindex, timestep_scheme, conditions, discretized
 import ...ImmersedBodies.Bodies: body_segment_length, bodypanels, prescribe_motion!
 
 using EllipsisNotation
@@ -15,7 +15,8 @@ using LinearMaps
 using StaticArrays
 using FunctionWrappers: FunctionWrapper
 
-export PsiOmegaFluidGrid, UniformGrid, MultiLevelGrid, gridstep, sublevel, baselevel
+export FreestreamFlow, PsiOmegaFluidGrid, UniformGrid, MultiLevelGrid
+export gridstep, sublevel, baselevel
 
 include("fluids.jl")
 